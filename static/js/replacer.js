1f44ca977169c64086a233ecf58c3894496cb841
Noel O'Boyle <baoilleach@gmail.com>
2025-01-23 16:14:44 +0000

    Add hover help text and use CXN SMILES for the attachment points

--- static/js/replacer.js
+++ static/js/replacer.js
@@ -18,6 +18,7 @@ var STATE = Backbone.Model.extend({
     "drawn": undefined,
     "smarts": undefined,
     "replace": undefined,
+    "hovertarget": undefined
   }
 });
 
@@ -26,6 +27,7 @@ state.on("change:drawn", StartSearch);
 state.on("change:searchtype", StartSearch);
 state.on("change:replace", Replace);
 state.on("change:help", HandleHelp);
+state.on("change:hovertarget", ShowDetails);
 
 // Start the Web worker
 worker = new Worker('static/js/worker.openbabel.js');
@@ -104,6 +106,11 @@ function HandleHelp()
   }
 }
 
+function TidySmiles(smi)
+{
+  return smi.replace(/\[R\]/g, "*").replace(/\*/g, "[#0]");
+}
+
 var Router = Backbone.Router.extend({
 
   routes: {
@@ -112,7 +119,7 @@ var Router = Backbone.Router.extend({
   },
 
   search: function(searchtype, smiles) {
-    if (document.JME.smiles().replace("[R]", "*").replace("*", "[#0]") != smiles) {
+    if (TidySmiles(document.JME.smiles()) != smiles) {
       var mol = RDKit.get_mol(smiles);
       var sdf = mol.get_molblock();
       document.JME.readMolFile(sdf);
@@ -134,10 +141,12 @@ function jsmeOnLoad() {
   // Add behaviour to JSME
   document.JME.setCallBack("AfterStructureModified", function(jsmeEvent) {
     var myjsme = jsmeEvent.src;
-    var navigation_url = "search/"+state.get("searchtype")+"/"+encodeURIComponent(myjsme.smiles().replace("[R]", "*").replace("*", "[#0]"))+"/";
+    var navigation_url = "search/"+state.get("searchtype")+"/"+encodeURIComponent(TidySmiles(myjsme.smiles()))+"/";
     console.log("Navigating to " + navigation_url);
     app.navigate(navigation_url, {trigger: true});
   });
+  $('#JME').mouseenter(function() {console.log("enter"); $('#jsme_help').show();})
+           .mouseleave(function() {console.log("leave");$('#jsme_help').hide();});
 }
 
 function Initialize()
@@ -166,7 +175,7 @@ function HandleResult(smi, molidx)
     state.set("terminate", true);
   }
   else {
-    var elem = $('<a href="/#replace/' + state.get("searchtype") + '/' + molidx + '" ><img data-molidx="' + molidx + '" src="https://www.simolecule.com/cdkdepict/depict/cow/png?abbr=off&hdisp=provided&disp=bridgehead&annotate=colmap&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(smi)) + '" /></a>\n')[0];
+    var elem = $('<a href="/#replace/' + state.get("searchtype") + '/' + molidx + '" ><img data-molidx="' + molidx + '" src="https://www.simolecule.com/cdkdepict/depict/cow/svg?abbr=off&hdisp=provided&disp=bridgehead&annotate=colmap&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(smi)) + '" /></a>\n')[0];
 
     if (state.get("first_result")) {
       state.set("first_result", false);
@@ -228,10 +237,10 @@ function Replace()
   var freq = freqs[molidx];
 
   var mhtml = [];
-  mhtml.push('<img id="query_0" src="https://www.simolecule.com/cdkdepict/depict/cow/svg?abbr=off&hdisp=provided&disp=bridgehead&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(smiles)) + '" />\n');
+  mhtml.push('<img id="query_img" data-freq="' + freq + '" src="https://www.simolecule.com/cdkdepict/depict/cow/svg?abbr=off&hdisp=provided&disp=bridgehead&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(smiles)) + '" />\n');
   mhtml.push('<br/>');
   mhtml.push('<span class="details frequency">' + freq + '</span>');
-  $('#query_img').html(mhtml.join(''));
+  $('#query_div').html(mhtml.join(''));
   for (var i=0; i<2; i++) {
     var section = (i==0) ? "before" : "after";
     var graph = (i==0) ? children : parents;
@@ -245,9 +254,9 @@ function Replace()
         var relmolidx_0 = (relmolidx > group_total) ? relmolidx - group_total : relmolidx;
         var relco = relatives[j][1];
         var relfreq = freqs[relmolidx_0];
-        var img_id = section + "_" + j;
+        var img_id = section + "_" + relmolidx;
         mhtml.push('<div>\n');
-        mhtml.push('<a href="/#replace/' + state.get("searchtype") + '/' + relmolidx_0 + '"><img id="' + img_id + '" src="https://www.simolecule.com/cdkdepict/depict/cow/svg?abbr=off&hdisp=provided&disp=bridgehead&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(idx2smi[relmolidx])) + '" /></a>\n');
+        mhtml.push('<a href="/#replace/' + state.get("searchtype") + '/' + relmolidx_0 + '"><img id="' + img_id + '" data-freq="' + relfreq + '" data-co="' + relco + '" src="https://www.simolecule.com/cdkdepict/depict/cow/svg?abbr=off&hdisp=provided&disp=bridgehead&showtitle=true&smi=' + encodeURIComponent(CXNSmiles(idx2smi[relmolidx])) + '" /></a>\n');
         mhtml.push('<br/>');
         mhtml.push('<span class="details">' + (j+1) + '.&nbsp;');
         mhtml.push('<span class="co-occurrence">' + relco + '</span>/<span class="frequency">'+ relfreq + '</span>');
@@ -259,9 +268,52 @@ function Replace()
   }
   $('#dv_before').removeClass("limbo");
   $('#dv_after').removeClass("limbo");
-  //$('img').mouseenter(HandleImgHover).mouseleave(HandleImgNoHover);
+  $('img').mouseenter(HandleImgHover).mouseleave(HandleImgNoHover);
   var mids = ['#before', '#after', '#title'];
   for (var i=0; i<mids.length; i++) {
-    //$(mids[i]).mouseenter(HandleImgHover).mouseleave(HandleImgNoHover);
+    $(mids[i]).mouseenter(HandleImgHover).mouseleave(HandleImgNoHover);
+  }
+}
+
+function HandleImgHover()
+{
+  state.set("hovertarget", $(this).attr('id'));
+}
+
+function HandleImgNoHover()
+{
+  state.set("hovertarget", undefined);
+}
+
+function ShowDetails()
+{
+  var targetid = state.get("hovertarget");
+  if (typeof(targetid) === "undefined") {
+     $('#helptext').html("");
+    return;
+  }
+
+  if (targetid == 'title') {
+    var text = ['Shown are those R groups that tend to be made around the same time as the query R group, divided into those that tend to be made before, versus those that tend to be made after.<br>This information is inferred from chemical structures text-mined from ChEMBL. It is based on co-occurrence information in matched series, rather than true project time-series data. To avoid over-counting, we only include a particular R group once per document.'];
+    text.push('<b>Key:</b><br><span class="frequency">Frequency in a matched series</span><br><span class="co-occurrence">Co-occurrence with the query</span>');
+    $('#helptext').html(text.join('<br>'));
+    return;
+  }
+  if (targetid == "after") {
+    $('#helptext').html('These R groups occur less frequently than the query and hence we infer that they are likely to be made <i>after</i> the query (if at all). They are ordered by co-occurrence with the query. Only the top 12 are shown.');
+    return;
+  }
+  if (targetid == "before") {
+    $('#helptext').html('These R groups occur more frequently than the query and hence we infer that they are likely to be made <i>before</i> the query (if at all). They are ordered by co-occurrence with the query divided by frequency. Only the top 12 are shown.');
+    return;
+  }
+  var parts = targetid.split('_');
+  var img = $('#' + targetid);
+  if (parts[0] == "query") {
+    $('#helptext').html('The query group occurs as part of a matched series in ' + img.attr("data-freq") + ' documents.');
+    return;
+  } else if (parts[0] == "after" || parts[0] == "before") {
+    $('#helptext').html('This group occurs as part of a matched series in ' + img.attr("data-freq") + ' documents. In ' + img.attr("data-co") + ' of those, it co-occurs with the query.');
+    return;
   }
 }
